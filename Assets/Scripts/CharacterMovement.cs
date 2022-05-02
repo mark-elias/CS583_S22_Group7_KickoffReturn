c215081b20693bab8b5915bccad84ffd98ea2c1b
Anthony Norderhaug <anorderhaug0480@sdsu.edu>
2022-05-02 12:15:28 -0700

    freelook fix & movement

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    private CharacterController controller;
     public Transform cam;
+    private Animator animator;
 
     public float walkingSpeed = 6f;
     public float runningSpeed = 12f;
@@ -19,14 +20,17 @@ public class CharacterMovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    void Start() {
+        // Cursor.lockState = CursorLockMode.Locked;
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-
-        // new Vector3(transform.forward * horizontal, 0f, transform.right * vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
@@ -51,10 +55,17 @@ public class CharacterMovement : MonoBehaviour
 
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir * targetSpeed * Time.deltaTime);
-            
-        }
+            Vector3 finalMove = moveDir * targetSpeed * Time.deltaTime;
 
+            if (finalMove.Equals(Vector3.zero)) {
+                animator.SetBool("moving", false);
+            } else {
+                animator.SetBool("moving", true);
+            }
+            controller.Move(moveDir * targetSpeed * Time.deltaTime);
+        } else {
+            animator.SetBool("moving", false);
+        }
     }
 }
 
